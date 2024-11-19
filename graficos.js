55cc0a99daf36af4d5e09635ab0a2c618d55c2ee
Luis Henrique Vieira Barros <162822478+oluisvi@users.noreply.github.com>
2024-11-19 14:39:45 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -93,3 +93,17 @@ new Chart(ctx2, {
         }
     }
 });
+
+const searchIcon = document.getElementById('search-icon');
+const searchOverlay = document.getElementById('search-overlay');
+
+searchIcon.addEventListener('click', () => {
+    searchOverlay.classList.toggle('active'); 
+    });
+
+    
+    document.addEventListener('click', (event) => {
+        if (!searchOverlay.contains(event.target) && !searchIcon.contains(event.target)) {
+            searchOverlay.classList.remove('active');
+        }
+    });
